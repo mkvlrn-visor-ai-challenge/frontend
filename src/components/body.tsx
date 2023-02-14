92df8c5bd01ad8dc16e5f8a38486e1cae41ad528
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 15:57:48 -0300

    feat: add styled esm call and placeholder msgs in chat area

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,9 +1,23 @@
 import { Container } from '@mantine/core';
 
+import { styled } from '#/components/_styled.jsx';
+
+const Messages = styled.div`
+  overflow-y: auto;
+
+  span {
+    display: block;
+  }
+`;
+
 export function Body() {
   return (
     <Container fluid style={{ marginTop: '4rem', marginBottom: '4rem' }}>
-      body
+      <Messages>
+        <span>placeholder msg#1</span>
+        <span>placeholder msg#2</span>
+        <span>placeholder msg#3</span>
+      </Messages>
     </Container>
   );
 }
