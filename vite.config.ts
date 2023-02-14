0f01100f5387c60b64934984157cf64244a193de
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 13:52:16 -0300

    chore:  add react, react-dom, adjust config for vite

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // @ts-expect-error, because of esm
+  plugins: [react()],
+  root: './src',
+  resolve: {
+    alias: {
+      '#': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: '../build',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
