fc42e6fc4d670b0cabdd2752e9ce6330b83266ae
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 13:59:26 -0300

    feat: basic index for react

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <p>hey</p>
+  </StrictMode>,
+);
