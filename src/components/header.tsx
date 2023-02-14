3ba248a764d9575233e2bb0b71faf2585ce2f7a9
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 20:29:26 -0300

    feat: tabler icons in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 import { Center, Header as MantineHeader, Title } from '@mantine/core';
+import { IconRobot, IconUser } from '@tabler/icons-react';
 
 export function Header() {
   return (
     <MantineHeader height='3rem'>
       <Center h='100%'>
-        <span className='fa-solid fa-user' />
-        <Title size='xl' m='sm'>
+        <IconUser />
+        <Title m='sm' order={2}>
           mkvlrn - visor.ai challenge
         </Title>
-        <span className='fa-solid fa-robot' />
+        <IconRobot />
       </Center>
     </MantineHeader>
   );
