04ea7b99c3d04eec94bd66c61251a67f7f33bf6a
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 15:26:06 -0300

    feat: add initial msg form in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,12 @@
-import { Footer as MantineFooter } from '@mantine/core';
+import { Button, Group, Footer as MantineFooter, TextInput } from '@mantine/core';
 
 export function Footer() {
-  return <MantineFooter height='3rem'>footer</MantineFooter>;
+  return (
+    <MantineFooter height='3rem'>
+      <Group position='center' align='center' h='100%'>
+        <TextInput placeholder='your message' style={{ width: '70%' }} />
+        <Button ml='xs'>send</Button>
+      </Group>
+    </MantineFooter>
+  );
 }
