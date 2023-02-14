863dd50e430c0d84cd70b418fd939010b0948b01
Mike Valeriano <mkvlrn@gmail.com>
2023-02-14 16:30:30 -0300

    feat: add recoilroot wrapper to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import { MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
 import { App } from '#/app/app.jsx';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <App />
-    </MantineProvider>
+    <RecoilRoot>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <App />
+      </MantineProvider>
+    </RecoilRoot>
   </StrictMode>,
 );
