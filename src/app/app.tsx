3d4d014f9bedeb8a638dc2bd8f7c4c5edb0a07fd
Mike Valeriano <mkvlrn@gmail.com>
2023-02-19 19:45:18 -0300

    feat: re enable backend, remove warning modal

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
-import { AppShell, Modal, Text } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { AppShell } from '@mantine/core';
+import { useEffect } from 'react';
 import { v4 as uuidV4 } from 'uuid';
 
 import { Body } from '#/components/body.jsx';
@@ -9,13 +9,6 @@ import { useChatState } from '#/recoil/state.js';
 
 export function App() {
   const { setChatId, setMessages } = useChatState();
-  const [warningModal, setWarningModal] = useState(true);
-
-  const email = (
-    <Text span weight='bold'>
-      mkvlrn@gmail.com
-    </Text>
-  );
 
   useEffect(() => {
     setChatId(uuidV4());
@@ -24,12 +17,6 @@ export function App() {
 
   return (
     <AppShell header={<Header />} footer={<Footer />} padding='md'>
-      <Modal opened={warningModal} onClose={() => setWarningModal(false)} title='backend offline'>
-        <Text>
-          hey guys! i took the backend offline for a while because of openai costs, but let me know
-          if you want it back online for any reason! drop me a line at {email}. later!
-        </Text>
-      </Modal>
       <Body />
     </AppShell>
   );
